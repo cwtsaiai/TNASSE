a0f8a97747459ad0345682f01729a6d6195fc0d1
ianlinus6102 <72379617+ianlinus6102@users.noreply.github.com>
2023-04-10 21:16:11 +0800

    Update search.sh

--- search.sh
+++ search.sh
@@ -1,3 +1,3 @@
-python3 main.py --algo 'se' --runs 50 --max_evaluations 1000--n 1 --h 3 --w 1 --sl 21 --ptype 'nasbench101' --atom 3 --score_function 'ni' --sigma 1
+python3 main.py --algo 'se' --runs 50 --max_evaluations 1000 --n 1 --h 3 --w 1 --sl 21 --ptype 'nasbench101' --atom 3 --score_function 'ni' --sigma 1
 python3 main.py --algo 'se' --runs 50 --max_evaluations 1000 --n 1 --h 3 --w 1 --sl 6 --ptype 'nasbench201' --atom 5 --score_function 'ni' --sigma 1
 python3 main.py --algo 'se' --runs 50 --max_evaluations 1000 --n 1 --h 3 --w 1 --sl 5 --ptype 'natsbenchsss' --atom 8 --score_function 'ni' --sigma 1
