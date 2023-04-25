378efb77dff6d9272ae42755fe2cbc54635c91e7
MarttiWu <69631800+MarttiWu@users.noreply.github.com>
2023-04-25 16:57:17 +0800

    Update search.sh

--- search.sh
+++ search.sh
@@ -1,3 +1,9 @@
+# TNASSE
 python3 main.py --algo 'se' --runs 50 --max_evaluations 1000 --n 1 --h 3 --w 1 --sl 21 --ptype 'nasbench101' --atom 3 --score_function 'ni' --sigma 1
 python3 main.py --algo 'se' --runs 50 --max_evaluations 1000 --n 1 --h 3 --w 1 --sl 6 --ptype 'nasbench201' --atom 5 --score_function 'ni' --sigma 1
 python3 main.py --algo 'se' --runs 50 --max_evaluations 1000 --n 1 --h 3 --w 1 --sl 5 --ptype 'natsbenchsss' --atom 8 --score_function 'ni' --sigma 1
+
+# NI
+python3 main.py --algo 'rs' --runs 50 --max_evaluations 1000 --sl 21 --ptype 'nasbench101' --atom 3 --score_function 'ni' --sigma 1
+python3 main.py --algo 'rs' --runs 50 --max_evaluations 1000 --sl 6 --ptype 'nasbench201' --atom 5 --score_function 'ni' --sigma 1
+python3 main.py --algo 'rs' --runs 50 --max_evaluations 1000 --sl 5 --ptype 'natsbenchsss' --atom 8 --score_function 'ni' --sigma 1
